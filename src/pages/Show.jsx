b876e336ed171d1b8c07678c61705942144873bd
Ali-Al-Noufal <alialnoufal3@gmail.com>
2026-02-16 09:12:19 -0500

    workDone

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -37,7 +37,7 @@ const Show = () => {
       }
     }).then(res=>setProject(res.data))
     .catch(err=>console.log(err))
-  },[project])
+  },[])
     
   return (
     <div className=" max-w-screen overflow-hidden dark:bg-[#1B2431]">
